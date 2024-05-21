cdf0cb8ada92cf7abd921bb977acafd8f50952e8
ManulaGunatilleke <inner.gunatilleke@gmail.com>
2024-05-21 16:32:50 +0530

    Updates for Profile Image

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -1,11 +1,14 @@
+import Image from 'next/image';
+import profilePic from "../../public/image/profile.jpeg";
+
 export const personalData = {
   name: "Manula Gunatilleke",
-  profile: '/profile.jpeg',
+  profile: profilePic.src,
   designation: "Software Developer",
   description: "My name is Manula Gunatilleke. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
   email: 'manula.gunatilleke22@gmail.com',
   phone: '+94705883826',
-  address: 'No: 110, Temple Read, Kalutara North',
+  address: 'No: 110, Temple Road, Kalutara North',
   github: 'https://github.com/ManulaGunatilleke',
   facebook: 'https://www.facebook.com/manula.gunatilleke/',
   linkedIn: 'https://www.linkedin.com/in/manula-gunatilleke/',
@@ -14,4 +17,15 @@ export const personalData = {
   leetcode: "https://leetcode.com/user7037Wi/",
   devUsername: "",
   resume: "https://drive.google.com/file/d/118C4H0UksZGTOG0hZbKEA7oC1yTSxkpW/view?usp=sharing"
+};
+
+// Usage in a React Component
+export default function Profile() {
+  return (
+    <div>
+      <h1>{personalData.name}</h1>
+      <Image src={personalData.profile} alt="Profile Picture" width={200} height={200} />
+      <p>{personalData.description}</p>
+    </div>
+  );
 }
